faaaef04545bf0cb44fb736537e8b70bb9e9dcdf
Tobias <123824117+josecostatobias@users.noreply.github.com>
2023-02-17 15:58:04 -0300

    novos controles e, nova plataforma

--- pag/pag_game/game.js
+++ pag/pag_game/game.js
@@ -1,22 +1,42 @@
 var x = 0;
 var y = 0;
-function moverparabaixo() {
-    x = x + 20
 
-    document.querySelector(".quadrado").style.marginTop = `${x}px`;
-}
-function moverparacima() {
-    x = x - 20
+//Window -> document -> resto do DOM
+// controle play 1
 
-    document.querySelector(".quadrado").style.marginTop = `${x}px`;
+document.addEventListener("keydown", function (evento) {
+    if (evento.key === "ArrowUp") {
+        moverParaCima();
+        console.log("cima");
+    }
+    if (evento.key === "ArrowDown") {
+        moverParaBaixo();
+        console.log("baixo");
+    }
+    if (evento.key === "ArrowLeft") {
+        moverParaEsqueda();
+        console.log("esqueda")
+    }
+    if (evento.key === "ArrowRight") {
+        moverParaDireita();
+        console.log("direita")
+    }
+})
+//controle play 2
+function moverParaCima() {
+    x = x - 50
+    document.querySelector(".quadradoY").style.marginTop = `${x}px`;
+}
+function moverParaBaixo() {
+    x = x + 50
+    document.querySelector(".quadradoY").style.marginTop = `${x}px`;
+} 
+function moverParaEsqueda() {
+    y = y - 50
+    document.getElementById("quadradoX").style.marginLeft = `${y}px`;
 }
 
-function moverparadireita() {
-    y = y + 20
-    document.getElementById("quadrado1").style.marginLeft = `${y}px`;
-}
-function moverparaesqueda() {
-    y = y - 20
-    
-    document.getElementById("quadrado1").style.marginLeft = `${y}px`;
+function moverParaDireita() {
+    y = y + 50
+    document.getElementById("quadradoX").style.marginLeft = `${y}px`;
 }
